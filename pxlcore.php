e59c1733109e6652585e3294c10779dca555e787
Mark Wilkinson <mark@markwilkinson.me>
2013-08-26 17:39:00 +0100

    Updated Text Domain
    Was set as pj changed to pxlcore

--- pxlcore.php
+++ pxlcore.php
@@ -59,16 +59,16 @@ function pxlcore_comments( $comment, $args, $depth ) {
 		<div id="comment-<?php comment_ID(); ?>">
             <div class="comment-author vcard">
                 <?php echo get_avatar( $comment, 40 ); ?>
-                <?php printf( __( '%s <span class="says">says:</span>', 'pj' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+                <?php printf( __( '%s <span class="says">says:</span>', 'pxlcore' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
             </div><!-- .comment-author .vcard -->
             <?php if ( $comment->comment_approved == '0' ) : ?>
-                <em><?php _e( 'Your comment is awaiting moderation.', 'pj' ); ?></em>
+                <em><?php _e( 'Your comment is awaiting moderation.', 'pxlcore' ); ?></em>
                 <br />
             <?php endif; ?>
             <div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
                 <?php
                     /* translators: 1: date, 2: time */
-                    printf( __( '%1$s at %2$s', 'pj' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'pj' ), ' ' );
+                    printf( __( '%1$s at %2$s', 'pxlcore' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'pxlcore' ), ' ' );
                 ?>
             </div><!-- .comment-meta .commentmetadata -->
             <div class="comment-body"><?php comment_text(); ?></div>
@@ -82,7 +82,7 @@ function pxlcore_comments( $comment, $args, $depth ) {
 		case 'trackback' :
 	?>
 	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'pj' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'pj'), ' ' ); ?></p>
+		<p><?php _e( 'Pingback:', 'pxlcore' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'pxlcore'), ' ' ); ?></p>
 	<?php
 			break;
 	endswitch;
